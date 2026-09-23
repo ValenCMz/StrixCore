8ad18533f4d680c3875d58d3dbad76c80f88acfc
Valentin Caminos Martinez <caminosmartinezvalentin@gmail.com>
2026-09-23 16:35:28 -0300

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,15 @@ export default function RootLayout({
         src="https://www.googletagmanager.com/gtag/js?id=G-TQ8TMXNJ6H"
         strategy="afterInteractive"
       />
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "ymybu9dnxx");
+        `}
+      </Script>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
